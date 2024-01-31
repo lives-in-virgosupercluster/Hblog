51d6371006da1cd85fa28a9e290f64e091714a9e
Harsh <harsh.singh0310@gmail.com>
2024-01-31 23:26:38 +0530

    made the home page

--- src/app/components/cardList/CardList.js
+++ src/app/components/cardList/CardList.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./cardlist.module.css"
 import Pagination from "../pagination/pagination";
-
+import { Card } from "../card/card";
  const CardList = () => {
   return (
-    <div className={styles.container}>Cardlist
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+      </div>
     <Pagination/>
     </div>
   )
--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,9 +1,49 @@
 import React from "react";
 import styles from "./footer.module.css"
+import Image from "next/image";
+import Link from "next/link";
 
  const Footer = () => {
   return (
-    <div className={styles.container}>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="lama blog" width={50} height={50}/>
+          <h1 className={styles.logoText}>Lamablog</h1>
+          </div>
+          <p className={styles.desc}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam voluptatum molestias facere facilis iste aperiam, doloribus laudantium unde quaerat sunt ipsa dicta quia voluptatibus aspernatur!</p>
+        </div>
+        <div className={styles.icon}>
+          <Image src="/facebook.png" alt="" width={18} height={18}></Image>
+          <Image src="/instagram.png" alt="" width={18} height={18}></Image>
+          <Image src="/tiktok.png" alt="" width={18} height={18}></Image>
+          <Image src="/youtube.png" alt="" width={18} height={18}></Image>
+        </div>
+        <div className={styles.links}>
+          <div className={styles.list}>
+            <span className={styles.listTitle} >Links</span>
+            <Link href="/">HomePage</Link>
+            <Link href="/">Blog</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
+          <div className={styles.list}>
+            <span className={styles.listTitle} >Tags</span>
+            <Link href="/">Style</Link>
+            <Link href="/">Fashion</Link>
+            <Link href="/">Coding</Link>
+            <Link href="/">Travel</Link>
+          </div>
+          <div className={styles.list}>
+            <span className={styles.listTitle} >Social</span>
+            <Link href="/">Facebook</Link>
+            <Link href="/">Instagram</Link>
+            <Link href="/">Youtube</Link>
+            <Link href="/">TikTok</Link>
+          </div>
+        </div>
+      </div>
+    
   )
 }
 export default Footer;
